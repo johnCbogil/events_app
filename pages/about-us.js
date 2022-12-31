431ca89aef5301de0405567e5597870350e9b97f
John Bogil <johncbogil@gmail.com>
2022-12-30 19:58:55 -0500

    initial commit

---
+++ pages/about-us.js
@@ -0,0 +1,12 @@
+export default function Page() {
+    return (
+        <>
+         <h1>About Us Page</h1>
+        <div>
+            <p>
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+        </>
+    )
+}
---
+++ pages/events.js
@@ -0,0 +1,17 @@
+export default function Events() {
+    return(
+        <div>
+        <h1>Events</h1>
+        <a href=''>
+          <img></img>
+          <h2>Events in London</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+          <h2>Events in San Francisco</h2>
+          <p>quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+          <h2>Events in Barcelona</h2>
+          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+        </a>
+        </div>
+    
+    )
+}
